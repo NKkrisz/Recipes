d040803b815bc15e0f15ff884f33d9914c6544c3
MountainSpiritQ <greenbull122@gmail.com>
2024-03-20 13:10:45 +0100

    Rename

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-async function getRecipes() {
+export async function getRecipes() {
     const response = await fetch("https://dummyjson.com/recipes");
     const data = await response.json();
     getNavbarBackground(data);
 }
 
-function getNavbarBackground(data) {
+export function getNavbarBackground(data) {
     const randomImage = data.recipes[Math.floor(Math.random() * data.recipes.length)].image;
     console.log(randomImage);
 }
