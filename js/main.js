ee321f7c97929f26c88ea7766df6757f05a5e646
MountainSpiritQ <greenbull122@gmail.com>
2024-03-20 13:20:17 +0100

    Randomnavbarimg

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-export async function getRecipes() {
+async function getRecipes() {
     const response = await fetch("https://dummyjson.com/recipes");
     const data = await response.json();
     getNavbarBackground(data);
 }
 
-export function getNavbarBackground(data) {
+function getNavbarBackground(data) {
     const randomImage = data.recipes[Math.floor(Math.random() * data.recipes.length)].image;
     console.log(randomImage);
+    document.querySelector('#nav').classList.add(`bg-[url('${randomImage}')]`)
 }
 
 getRecipes()
