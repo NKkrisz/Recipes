c1fb67507d38a834d0efbbf109a0191284a1071e
Atoti2 <bogyikormi3@gmail.com>
2024-03-20 14:37:36 +0100

    filter function finished

--- js/filterByTags.js
+++ js/filterByTags.js
@@ -4,33 +4,35 @@ export async function filterTags(func, query){
     func(data) 
 }
 
-
-
 let clicked
-function filteredTags(asd){
+function filteredTags(){
     document.querySelectorAll('.tagItem').forEach((item) => {
         item.addEventListener('click', ((data) => {
+            document.querySelectorAll('.tagItem').forEach((i) => {
+                i.classList.remove('bg-red-600')
+            })
+            data.target.classList.add('bg-red-600')
             clicked = data.target.innerHTML
             filterTags(renderCards, clicked)
-            console.log(asd);
         }))
     })
 }
 
 function renderCards(data) {
     const cardContainer = document.querySelector("#card-container");
+    cardContainer.innerHTML = ""
     data.recipes.forEach(recipe => {
-        console.log(recipe);
-        // const card = document.createElement("div");
-        // card.classList.add("card", "w-1/4", "bg-slate-800", "p-4", "rounded-lg", "shadow-lg", "text-white");
-        // card.innerHTML = `
-        //     <img src="${recipe.image}" alt="${recipe.name}" class="card-image/>
-        //     <div class="card-content">
-        //         <h2 class="card-title">${recipe.name}</h2>
-        //         <p class="card-description">${recipe.tags}</p>
-        //     </div>
-        // `;
-        // cardContainer.appendChild(card);
+        // console.log(recipe);
+        const card = document.createElement("div");
+        card.classList.add("card", "w-1/4", "bg-slate-800", "p-4", "rounded-lg", "shadow-lg", "text-white");
+        card.innerHTML = `
+            <img src="${recipe.image}" alt="${recipe.name}" class="card-image/>
+            <div class="card-content">
+                <h2 class="card-title">${recipe.name}</h2>
+                <p class="card-description">${recipe.tags}</p>
+            </div>
+        `;
+        cardContainer.appendChild(card);
     });
 }
 
