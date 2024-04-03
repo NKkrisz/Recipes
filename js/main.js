28448da92c7d95dc200ac98e7dbd678622ac9ff9
Atoti2 <bogyikormi3@gmail.com>
2024-04-03 08:40:17 +0200

    nothing has changed

--- js/main.js
+++ js/main.js
@@ -8,9 +8,8 @@ async function getRecipes() {
 
 function getNavbarBackground(data) {
     const randomImage = data.recipes[Math.floor(Math.random() * data.recipes.length)].image;
-    // console.log(randomImage);
     document.querySelector('#nav').classList.add(`bg-[url('${randomImage}')]`)
-   
+    
 }
 
 
